850768f24767867a9b2ab43bce08e1172f0d08e7
Gabriel <g2p.code@gmail.com>
2008-12-28 23:51:00 +0100

    Now with setuptools, no cruft.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      author='Gabriel de Perthuis',
+      author_email='g2p.code@gmail.com',
+      packages=['systems', ],
+      package_dir={'systems': 'src'}
+     )
