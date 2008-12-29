49278e0a3d308d008c3806b318841e21c113edb3
Gabriel <g2p.code@gmail.com>
2008-12-29 12:00:17 +0100

    Explicitly mention subpackages we want installed.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='Systems',
       version='0.1',
       author='Gabriel de Perthuis',
       author_email='g2p.code@gmail.com',
-      packages=['systems', ],
+      packages=['systems', 'systems.resources', ],
       package_dir={'systems': 'src'}
      )
