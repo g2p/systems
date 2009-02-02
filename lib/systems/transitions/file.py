084e71cf1f8b5dd7bf6a1d39f5aacd7a8e6a4089
Gabriel <g2p.code@gmail.com>
2009-02-03 00:50:00 +0100

    Style refresh for File resource type.

--- lib/systems/transitions/file.py
+++ lib/systems/transitions/file.py
@@ -12,72 +12,44 @@ from systems.util.syscalls import fchmod
 
 __all__ = ('register', )
 
+
+def is_valid_path(path):
+  """
+  Only absolute paths are accepted.
+  """
+
+  return os.path.isabs(path)
+
+def read_contents(id):
+  path = id.id_attrs['path']
+  with open(path) as f:
+    return f.read()
+
+def read_present(id_attrs):
+  """
+  Whether the file must exist.
+  """
+
+  path = id_attrs['path']
+  # Broken symlinks still are 'present'
+  return os.path.lexists(path)
+
+def is_valid_mode(mode):
+  # Only allow the permission bits.
+  # Disallow suid stuff for now.
+  return mode == (stat.S_IMODE(mode) & 0777)
+
+def read_mode(id):
+  path = id.id_attrs['path']
+  # May return invalid mode.
+  # I think it's ok; invalid modes may exist, we just don't set them.
+  return stat.S_IMODE(os.lstat(path).st_mode)
+
+
 class File(Resource):
   """
   A file in the filesystem.
   """
-
-  @classmethod
-  def register(cls):
-    cls.__restype = ResourceType('File', cls,
-        id_type={
-          'path': AttrType(
-            valid_condition=cls.is_valid_path),
-          },
-        state_type={
-          # Not specifying contents means the file will be emptied.
-          'contents': AttrType(
-            default_value='',
-            reader=cls.read_contents,
-            # A byte string, no encoding
-            pytype=str),
-          'present': AttrType(
-            default_value=True,
-            pytype=bool,
-            reader=cls.read_present),
-          'mode': AttrType(
-            default_value=0600,
-            reader=cls.read_mode,
-            valid_condition=cls.is_valid_mode),
-          })
-    Registry.get_singleton().resource_types.register(cls.__restype)
-
-  @classmethod
-  def is_valid_path(cls, path):
-    """
-    Only absolute paths are accepted.
-    """
-
-    return os.path.isabs(path)
-
-  @classmethod
-  def read_contents(cls, id):
-    path = id.id_attrs['path']
-    with open(path) as f:
-      return f.read()
-
-  @classmethod
-  def read_present(cls, id_attrs):
-    """
-    Whether the file must exist.
-    """
-
-    path = id_attrs['path']
-    # Broken symlinks still are 'present'
-    return os.path.lexists(path)
-
-  @classmethod
-  def is_valid_mode(cls, mode):
-    # Only allow the permission bits.
-    # Disallow suid stuff for now.
-    return mode == (stat.S_IMODE(mode) & 0777)
-
-  @classmethod
-  def read_mode(cls, id):
-    path = id.id_attrs['path']
-    # May return invalid mode.
-    # I think it's ok; invalid modes may exist, we just don't set them.
-    return stat.S_IMODE(os.lstat(path).st_mode)
 
   def place_transitions(self, transition_graph):
     code = transition('PythonCode', function=self._realize)
@@ -87,7 +59,7 @@ class File(Resource):
   def _realize(self):
     # Don't let files be accessible between creation and permission setting.
     os.umask(0077)
-    present0 = self.read_present(self.id_attrs)
+    present0 = self.read_attrs()['present']
     present1 = self.wanted_attrs['present']
 
     if present1:
@@ -100,5 +72,27 @@ class File(Resource):
       os.unlink(self.id_attrs['path'])
 
 def register():
-  File.register()
+  restype = ResourceType('File', File,
+      id_type={
+        'path': AttrType(
+          valid_condition=is_valid_path),
+        },
+      state_type={
+        # Not specifying contents means the file will be emptied.
+        'contents': AttrType(
+          default_value='',
+          reader=read_contents,
+          # A byte string, no encoding
+          pytype=str),
+        'present': AttrType(
+          default_value=True,
+          pytype=bool,
+          reader=read_present),
+        'mode': AttrType(
+          default_value=0600,
+          reader=read_mode,
+          valid_condition=is_valid_mode),
+        })
+  Registry.get_singleton().resource_types.register(restype)
+
 
